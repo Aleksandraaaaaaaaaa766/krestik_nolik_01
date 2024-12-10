dc9f259fdd5b3d579c476cc29e399dbea48ea38b
Toporik24 <nekitlenivec@gmail.com>
2024-12-10 10:25:56 +0500

    Update ВЕСЬ КОД.py

--- ВЕСЬ КОД.py
+++ ВЕСЬ КОД.py
@@ -2,7 +2,6 @@
 import tkinter as tk
 from tkinter import messagebox
 
-
 # Функция для проверки  победы
 def check_win(board, player):
     # Проверка горизонталей
@@ -18,15 +17,18 @@ def check_win(board, player):
         return True
     return False
 
+
 # Функция для проверки ничьей
 def check_draw(board):
     return all([cell != " " for row in board for cell in row])
+    
 # Функция для обработки хода игрока
 def player_move(row, col):
     global current_player
     if board[row][col] == " ":
         board[row][col] = current_player
         buttons[row][col].config(text=current_player, state="disabled")
+        
         # Проверка на победу
         if check_win(board, current_player):
             messagebox.showinfo("Победа!", f"Игрок {current_player} победил!")
@@ -39,6 +41,8 @@ def player_move(row, col):
             # Смена игрока
             # В переменную записывается О, если был Х, иначе Х
             current_player = "O" if current_player == "X" else "X"
+
+
 # Функция для сброса игры
 def reset_game():
     global board, current_player
@@ -49,13 +53,17 @@ def reset_game():
         for j in range(3):
             buttons[i][j].config(text=" ", state="normal")
     current_player = "X"  # Начинает всегда игрок X
+
+
 # Создание окна
 root = tk.Tk()
 root.title("Крестики-Нолики")
 # Инициализация глобальных переменных
+
 # создаётся поле 3*3 из пустых строк. работаем с матрицей
 board = [[" " for _ in range(3)] for _ in range(3)]
 current_player = "X"  # Игрок X начинает первым
+
 # Создание кнопок для игрового поля
 buttons = [[None for _ in range(3)] for _ in range(3)]
 for i in range(3):
@@ -63,8 +71,10 @@ for i in range(3):
         buttons[i][j] = tk.Button(root, text=" ", font=('normal', 40), width=5, height=2,
                                   command=lambda row=i, col=j: player_move(row, col))
         buttons[i][j].grid(row=i, column=j)
+        
 # Кнопка для сброса игры
 reset_button = tk.Button(root, text="Сбросить игру", font=('normal', 14), command=reset_game)
 reset_button.grid(row=3, column=0, columnspan=3)
+
 # Запуск игры
 root.mainloop()
