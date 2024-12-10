580affca459be1f02d87041afc5cd4c17a37988a
Aleksandraaaaaaaaaa766 <151709825+Aleksandraaaaaaaaaa766@users.noreply.github.com>
2024-12-10 10:22:50 +0500

    Update ВЕСЬ КОД.py

--- ВЕСЬ КОД.py
+++ ВЕСЬ КОД.py
@@ -3,7 +3,20 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-
+# Функция для проверки  победы
+def check_win(board, player):
+    # Проверка горизонталей
+    for row in board:
+        if all([cell == player for cell in row]):
+            return True
+    # Проверка вертикалей
+    for col in range(3):
+        if all([board[row][col] == player for row in range(3)]):
+            return True
+    # Проверка диагоналей
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
 
 # Функция для проверки ничьей
 def check_draw(board):
